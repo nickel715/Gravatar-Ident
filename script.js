c01745a1d3a853f1c475a25a02375cc5c7300736
Nicolas Hohm <nicolashohm@gmail.com>
2013-07-23 22:56:15 +0200

    fixed #1 ajax problem

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
-var images = document.getElementsByTagName('img');
+function replaceAvatars() {
+    var images = document.getElementsByTagName('img');
 
-for (var i = images.length - 1; i >= 0; i--) {
+    for (var i = images.length - 1; i >= 0; i--) {
 
-    var src = images[i].getAttribute('src');
+        var src = images[i].getAttribute('src');
 
-    if (src.indexOf('gravatar.com') !== -1) {
+        if (src.indexOf('gravatar.com') !== -1) {
 
-        var newUrl = src.replace('d=mm', 'd=identicon');
-        images[i].setAttribute('src', newUrl);
+            var newUrl = src.replace('d=mm', 'd=identicon');
+            images[i].setAttribute('src', newUrl);
+
+        }
 
     }
-
 }
+
+replaceAvatars();
+
+document.addEventListener("DOMSubtreeModified", function() {
+    replaceAvatars();
+}, false);
